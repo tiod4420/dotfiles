d4f6520c1341b0e844c2e3c845fcccb5797f3a87
Jonas Lejeune <jonas.lejeune4420@gmail.com>
2023-11-19 17:33:31 +0900

    Add alias to open TODO file

--- .bash.d/aliases.sh
+++ .bash.d/aliases.sh
@@ -55,10 +55,8 @@ aliases()
 	alias asn1parse="openssl asn1parse -i -dump"
 	alias b64enc="openssl base64"
 	alias b64dec="openssl base64 -d"
+	alias rand="openssl rand"
 	alias x509="openssl x509 -noout -text"
-
-	# Python one-liners
-	alias pol="python -c"
 
 	# Convert hex to binary and reverse
 	alias tobin="xxd -p -r"
@@ -71,10 +69,14 @@ aliases()
 	# Allow display of raw characters (only ANSI colors) with less
 	alias less="less -R"
 
-	# Print each PATH entry on a separate line
-	alias path='echo -e ${PATH//:/\\n}'
 	# Map command to one argument
 	alias map='xargs -n1'
+	# Print each PATH entry on a separate line
+	alias path='echo -e ${PATH//:/\\n}'
+	# Python one-liners
+	alias pol="python -c"
+	# Edit todo file
+	alias todo="vim \$TODO_FILE"
 
 	# Use Git’s colored diff when available
 	command -v git &> /dev/null && alias gdiff="git diff --no-index"
