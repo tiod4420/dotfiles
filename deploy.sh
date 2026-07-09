a78d348dac2df0c85e680626cc631113b07931d6
Jonas Lejeune <jonas.lejeune4420@gmail.com>
2026-07-09 12:08:55 +0900

    Improve deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,39 +4,51 @@
 
 set -Eeuo pipefail
 
-# XDG config directory
-XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+# Display error message, usage, and quit
+error() {
+	local msg=${1:-error}
+	local ret=${2:-1}
+
+	echo "$0: $msg" >&2
+
+	exit $ret
+}
 
 # Deploy a file or a directory to its location
 deploy() {
 	local src=$1
+	local depth=${2:-2}
 	local file
-	local file2
 
-	if [ -f "$src" ]; then
-		# Deploy single file
+	# Check that src is valid
+	! [ -f "$src" ] && ! [ -d "$src" ] && error "'$src': No such file or directory"
+
+	# End recursion if regular file or depth is 0
+	if [ -f "$src" ] || [ "$depth" -eq 0 ]; then
 		deploy_target "$src"
-	else
-		# Deploy a directory
-		for file in $src/*; do
-			if [ -f "$file" ]; then
-				# Single files
-				deploy_target "$file"
-			else
-				# Recurse one level in the directory
-				for file2 in $file/*; do
-					deploy_target "$file2"
-				done
-			fi
-		done
+		return
 	fi
 
-	return 0
+	# Recurse into directory
+	for file in "$src"/*; do
+		# Check that file exists in case of empty directory
+		[ -e "$file" ] && deploy "$file" "$((depth - 1))"
+	done
 }
 
 # Create a symlink if destination doesn't exists
 deploy_symlink() {
-	! [ -e "$1" ] && ln -s "$1" "$2"
+	local target=$1
+	local link=$2
+
+	! [ -e "$target" ] && error "'$target': No such file or directory"
+
+	if ! [ -e "$link" ]; then
+		run ln -s "$target" "$link"
+		print_status "$link" "DEPLOYED"
+	else
+		print_status "$link" "SAME"
+	fi
 }
 
 # Deploy a source file/directory to a destination
@@ -45,10 +57,10 @@ deploy_target() {
 	local src=$1
 	local dst
 	local parent
-	local action
 	local prompt
 	local choice
 
+	# Check that src is valid
 	! [ -e "$src" ] && return 1
 
 	# Set the destination to HOME or XDG_CONFIG_HOME
@@ -61,8 +73,8 @@ deploy_target() {
 	if ! [ -e "$dst" ]; then
 		# Destination does not exist, create parent directory and copy source
 		parent=$(dirname "$dst")
-		! [ -e "$parent" ] && mkdir -p "$parent"
-		cp -r "$src" "$dst"
+		! [ -e "$parent" ] && run mkdir -p "$parent"
+		run cp -r "$src" "$dst"
 		print_status "$dst" "DEPLOYED"
 	elif git diff --no-index --quiet "$dst" "$src" &> /dev/null; then
 		# Source and destination are the same
@@ -72,8 +84,11 @@ deploy_target() {
 		print_status "$dst" "DIFF"
 
 		# Make prompt
-		[ -f "$src" ] && action="overwrite file" || action="replace directory"
-		prompt="Do you want to ${action} '$(basename "$dst")'? [y/N/d/q] "
+		prompt="Do you want to "
+		[ -f "$src" ] && prompt+="overwrite file" || prompt+="replace directory"
+		prompt+=" '"
+		prompt+="$(print_color bold green)$(basename "$dst")$(print_color reset)"
+		prompt+="'? [y/N/d/q] "
 
 		# Get user choice
 		while true; do
@@ -83,8 +98,8 @@ deploy_target() {
 			case "${choice,,}" in
 				y|yes)
 					# Replace destination
-					rm -rf "$dst"
-					cp -r "$src" "$dst"
+					run rm -rf "$dst"
+					run cp -r "$src" "$dst"
 					print_status "$dst" "REPLACED"
 					break
 					;;
@@ -113,24 +128,91 @@ deploy_target() {
 	return 0
 }
 
+has_cmd() {
+	command -v "$1" &> /dev/null
+}
+
+# Print color code if supported
+print_color() {
+	local color
+	local ncolors=$(tput colors 2> /dev/null)
+	local -A colors=(
+		[reset]=0
+		[bold]=1
+		[dim]=2
+		[black]=30
+		[red]=31
+		[green]=32
+		[yellow]=33
+		[blue]=34
+		[magenta]=35
+		[cyan]=36
+		[white]=37
+	)
+
+	# Color mode not supported
+	! [ -z "${NO_COLOR:-}" ] && return
+	! [ "${ncolors:-0}" -ge 16 ] && return
+
+	for color in "$@"; do
+		echo -ne "\e[${colors[$color]:-}m"
+	done
+}
+
 # Print header for deploying of a program
 print_deploy() {
-	local version=$(version_get "$1")
-	echo "Deploying $1 configuration -- ${version:+version }${version:-NOT FOUND}"
+	local cmd=${1:-}
+	local version=$(version "$cmd")
+
+	echo -n "$(print_color bold)Deploying "
+	echo -n "$(print_color blue)$cmd$(print_color reset bold)"
+	echo -n " configuration -- "
+	if [ -n "$version" ]; then
+		echo -n "version $(print_color blue)${version}"
+	else
+		echo -n "$(print_color yellow)NOT FOUND"
+	fi
+	echo "$(print_color reset)"
 }
 
 # Print action taken for deploying a file
 print_status() {
-	echo "    ${1/#"$HOME"/"~"} ... ${2:-}"
+	local path=${1/#"$HOME"/"~"}
+	local status=${2:-}
+
+	if [ -n "${DRY_RUN:-}" ]; then
+		case "$status" in
+			DEPLOYED|REPLACED) status="DRY RUN" ;;
+		esac
+	fi
+
+	echo -n "    $path ... "
+
+	case "${status:-}" in
+		DEPLOYED) echo -n "$(print_color green)";;
+		DIFF) echo -n "$(print_color bold yellow)" ;;
+		DRY\ RUN) echo -n "$(print_color magenta)" ;;
+		QUIT) echo -n "$(print_color red)" ;;
+		REPLACED) echo -n "$(print_color cyan)" ;;
+		SKIP) echo -n "$(print_color dim)" ;;
+	esac
+
+	echo -n "$status"
+	echo "$(print_color reset)"
+}
+
+# Run command only if not dry-run mode
+run() {
+	[ -z "${DRY_RUN:-}" ] && "$@" || true
 }
 
 # Get a normalized version of a program
-version_get() {
+version() {
 	local cmd=$1
 	local regex='[0-9]+(\.[0-9]+)+'
 
 	# Check if command exists
-	! command -v "$cmd" &> /dev/null && return 0
+	! has_cmd "$cmd" && return 0
 
 	# Extract version
 	case "$cmd" in
@@ -145,9 +227,7 @@ version_get() {
 		tldr) tldr --version | grep -Eo "$regex" ;;
 		tmux) tmux -V | grep -Eo "${regex}[a-z]" ;;
 		vim) vim --version | head -n 1 | grep -Eo "$regex" ;;
-	esac
-
-	return 0
+	esac || true
 }
 
 # Return true if the lhs version is strictly older than the rhs
@@ -157,11 +237,16 @@ version_lt() {
 	[ -n "$1" ] && [ "$1" != "$2" ] && [ "$1" = "$oldest" ]
 }
 
+# Global variables
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+
+# Set dry run
+case "${1:-}" in
+	-d|--dry-run) DRY_RUN=true && shift ;;
+esac
+
 # Check that git exists
-if ! command -v git &> /dev/null; then
-	echo "git: command not found"
-	exit 1
-fi
+! has_cmd git && error "git: command not found"
 
 # Initialize and update git submodules
 git submodule update --init --recursive
@@ -190,14 +275,9 @@ deploy .clang-format
 echo ""
 
 # GDB
-version=
 print_deploy gdb
 deploy .config/gdb
-
-if version_lt "$(version_get gdb)" 11.1; then
-	deploy_symlink "$XDG_CONFIG_HOME/gdb/gdbinit" "$HOME/.gdbinit"
-fi
-
+version_lt "$(version gdb)" 11.1 && deploy_symlink "$XDG_CONFIG_HOME/gdb/gdbinit" "$HOME/.gdbinit"
 echo ""
 
 # Git
@@ -212,7 +292,7 @@ echo ""
 
 # SSH
 print_deploy ssh
-! [ -d "$HOME/.ssh" ] && mkdir --mode 700 "$HOME/.ssh"
+! [ -d "$HOME/.ssh" ] && run mkdir --mode 700 "$HOME/.ssh"
 deploy .ssh
 echo ""
 
@@ -229,13 +309,8 @@ echo ""
 # Vim
 print_deploy vim
 deploy .config/vim
-
-if version_lt "$(version_get vim)" 9.2; then
-	deploy_symlink "$XDG_CONFIG_HOME/vim" "$HOME/.vim"
-fi
-
+version_lt "$(version vim)" 9.2 && deploy_symlink "$XDG_CONFIG_HOME/vim" "$HOME/.vim"
 echo ""
 
 # End of deployment
 echo "Deployment completed successfully!"
-exit 0
