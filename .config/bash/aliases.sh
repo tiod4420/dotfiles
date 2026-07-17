549e68bde63bc773901e8169b3e35bfafd87dfb1
Jonas Lejeune <jonas.lejeune4420@gmail.com>
2026-07-17 19:20:02 +0900

    Add aliases and functions to bashrc

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -2,33 +2,25 @@
 #
 # Alias settings
 
-# Easier navigation
+# cd aliases
 alias ..='cd ..'
 alias ...='cd ../..'
-alias gd='cd $(git rev-parse --show-toplevel || echo .)'
+alias ....='cd ../../..'
+alias groot='cd $(git rev-parse --show-toplevel 2> /dev/null || pwd)'
 
-# Safe cp and mv
+# cp and mv aliases
 alias cp='cp -i'
 alias mv='mv -i'
 
 # ls aliases
 alias ls='ls --color=auto'
 alias ll='ls -lh'
-alias la='ll -a'
+alias la='ll -A'
 
-# Colors aliases
-alias diff='diff --color=auto'
-alias grep='grep --color=auto'
-alias ip='ip --color=auto'
-
-# Find aliases
+# find aliases
 alias ff='find . -name'
 alias fh='history | grep'
 alias ft='grep -rIi --exclude-dir={.git,build,target}'
-
-# PATH and MANPATH aliases
-alias lsman='man --path | tr ":" "\n"'
-alias lspath='echo $PATH | tr ":" "\n"'
 
 # gdb aliases
 alias gef='gdb -ex start-gef'
@@ -45,32 +37,54 @@ alias x509='openssl x509 -text -noout'
 alias lstar='tar tvf'
 alias untar='tar xvf'
 
-# Count occurences of similar lines
-alias count='(sort | uniq -c | sort -nr)'
-# Hexdump of data (can be reversed with -r)
-alias dump='xxd -g 1'
+# tr aliases
+alias gu='tr "[:upper:]" "[:lower:]"'
+alias gU='tr "[:lower:]" "[:upper:]"'
+alias g~='tr "[:upper:][:lower:]" "[:lower:][:upper:]"'
+alias rot13='tr "A-Za-z" "N-ZA-Mn-za-m"'
+alias trim='sed "s/^[[:space:]]*//; s/[[:space:]]*$//"'
+
+# Colors aliases
+alias diff='diff --color=auto'
+alias grep='grep --color=auto'
+alias ip='ip --color=auto'
+
+# PATH and MANPATH aliases
+alias lsman='man --path | tr ":" "\n"'
+alias lspath='echo $PATH | tr ":" "\n"'
+
+# List aliases
+alias aliases='alias | sed "s/^alias[[:space:]]*\([^[:space:]]*\)='\''\(.*\)'\''$/\1 = \2/" | sort'
+# Remove duplicates while preserving order
+alias dedup='awk "!seen[\$0]++"'
+# Flip line order
+alias flip='tac'
 # Git diff out of repository
-alias ffid='git diff --no-index'
-# Filter file to keep only last extension
-alias fileext='sed -nE "s/^.*[^/]\.([^./]+)$/\1/p"'
+alias gdiff='git diff --no-index'
 # List history commands without prefix number
-alias hist='history | sed -nE "s/^[[:space:]]*[0-9]+[[:space:]]+//p"'
+alias hist='history | sed "s/^[[:space:]]*[0-9]*[[:space:]]*//"'
 # Spawn a HTTP server on current directory
 alias http='python3 -m http.server'
 # Map list of arguments
 alias map='xargs -n1'
-# Remove dupplicated lines while keeping order
-alias nodupes='(cat -n | sort -k 2 -u | sort -k 1 -n | cut -f 2-)'
-# ROT13 data
-alias rot13='tr "[:upper:][:lower:]" "N-ZA-Mn-za-m"'
-# List unique lines only
-alias soun='sort | uniq'
+# Make a QR code
+alias qr='qrencode -t ANSIUTF8'
 # Search for SyncThing conflicts
 alias syncoops='[ -d "$SYNCTHING_DIR" ] && find $SYNCTHING_DIR -name "*.sync-conflict*"'
 # Search for TODOs
 alias todo='ft "\<TODO\>"'
-# Trim line from front and back spaces
-alias trim='sed -nE "s/^[[:space:]]*(.*[^[:space:]])[[:space:]]*$/\1/p"'
+
+# Clipboard aliases
+if _bashrc_has_cmd pbcopy; then
+	alias cbcopy='pbcopy'
+	alias cbpaste='pbpaste'
+elif _bashrc_has_cmd wl-copy; then
+	alias cbcopy='wl-copy --type "text/plain;charset=utf-8"'
+	alias cbpaste='wl-paste'
+elif _bashrc_has_cmd xclip; then
+	alias cbcopy='xclip -selection clipboard -i'
+	alias cbpaste='xclip -selection clipboard -o'
+fi
 
 # Normalize open across Linux and macOS
 if ! _bashrc_has_cmd open; then
--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -7,37 +7,122 @@ calc() {
 	python -c "from math import *; print($*)"
 }
 
-# Search for files without NL at end of file
-crlf() {
-	local dir=${1:-.}
-	find "$dir" -type f -exec grep -q "$(printf \\r)" {} \; -print
+# Count occurences of similar lines
+count() {
+	sort "$@" | uniq -c | sort -nr
+}
+
+# Drop first or last n lines
+drop() {
+	local OPTIND
+	local OPTARG
+	local opt
+	local n
+
+	# Parse options
+	while getopts ":n:" opt; do
+		case "$opt" in
+			n) n=$OPTARG ;;
+			:) echo "$FUNCNAME: option requires an argument -- $OPTARG" && return 1 ;;
+			?) echo "$FUNCNAME: illegal option -- $OPTARG" && return 1 ;;
+		esac
+	done
+
+	shift $((OPTIND - 1))
+
+	# Same default as head or tail
+	[ -z "${n:-}" ] && n=10
+
+	# Drop lines
+	case "$n" in
+		-*) head -n $n -- "$@" ;;
+		*) tail -n +$((n + 1)) -- "$@" ;;
+	esac
+}
+
+# List functions names or specific function implementatioe
+functions() {
+	if [ "$#" -eq 0 ]; then
+		declare -F | sed -e 's/^declare -f //' -e '/^_/d'
+	else
+		declare -f "$1"
+	fi
 }
 
 # Convert Epoch timestamp to date or reverse
 epoch() {
 	local mode=epoch
 
-	# Parse parameters
+	# Parse options
 	case "${1:-}" in
 		-r | --revert) mode=revert && shift ;;
 	esac
 
+	# Get epoch
 	case "$mode" in
-		epoch) date ${1:+--date "$1"} +%s ;;
-		revert) date ${1:+--date @"$1"} +%FT%T ;;
+		epoch) date ${1:+--date "${1:-}"} +%s ;;
+		revert) date ${1:+--date @"${1:-}"} +%FT%T ;;
 	esac
+}
+
+# Get file name or extension
+filename() {
+	local mode=name
+	local name
+	local ext
+
+	# Parse options
+	while [ "$#" -gt 0 ]; do
+		case "$1" in
+			-e | --ext) mode=ext && shift ;;
+			--) shift && break ;;
+			-*) echo "$FUNCNAME: illegal option -- ${1:-}" >&2 && return 1 ;;
+			*) break ;;
+		esac
+	done
+
+	if [ -z "$1" ]; then
+		echo "$FUNCNAME: invalid usage" >&2
+		return 1
+	fi
+
+	# Get filename (without initial . if hidden file)
+	name=$(basename -- "$1")
+
+	# Normalize hidden files and get extension
+	ext=$(echo "$name" | sed -n -e 's/^\.//' -e 's/.*\(\.[^.]*\)$/\1/p')
+
+	case "$mode" in
+		name) basename "$name" "$ext" ;;
+		ext) [ -n "$ext" ] && echo "$ext" ;;
+	esac
+}
+
+# Convert or revert to hex
+hex() {
+	local mode=hex
+
+	# Parse options
+	case "${1:-}" in
+		-r | --revert) mode=revert && shift ;;
+	esac
+
+	# Get hex value
+	case "$mode" in
+		hex) xxd -g1 "$@" ;;
+		revert) xxd -r "$@" ;;
+	esac
+}
+
+# Make a directory and cd into it
+mkcd() {
+	[ -n "${1:-}" ] && mkdir -p -- "$1" && cd "$1"
 }
 
 # Make a .tar.gz archive from a list of anything
 mktar() {
 	local name=$(basename "${1:-}")
 	[ -n "$name" ] && tar czvf "$name.tar.gz" "$@"
-}
-
-# Search for files without NL at end of file
-nonl() {
-	local dir=${1:-.}
-	find "$dir" -type f -not -exec sh -c '[ -z "$(tail -c1 $0)" ]' {} \; -print
 }
 
 # Open notes directory
@@ -55,6 +140,20 @@ notes() {
 	vim "$dir/$file"
 }
 
+# Display today's date in sort of ISO 8601 format
+now() {
+	local format="%F"
+
+	# Parse options
+	case "${1:-}" in
+		-f | --full) format="%FT%T" && shift ;;
+		-i | --iso) format="%FT%T%:z" && shift ;;
+	esac
+
+	# Display date
+	date ${1:+--date "$1"} +"$format"
+}
+
 # Plot data with gnuplot
 plot() {
 	local mode=lines
@@ -68,37 +167,20 @@ plot() {
 	# Parse options
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
-			-g | --gui)
-				terminal="qt persist"
-				shift
-				;;
-			-h | --histogram)
-				mode=histogram
-				shift
-				;;
+			-g | --gui) terminal="qt persist" && shift ;;
+			-h | --histogram) mode=histogram && shift ;;
 			-r | --range)
 				if [ -z "${2:-}" ]; then
-					echo "$FUNCNAME: option $1 requires an argument" >&2
+					echo "$FUNCNAME: option requires an argument -- ${1:-}" >&2
 					return 1
 				fi
 				range=$2
 				shift 2
 				;;
-			-z | --zero)
-				yrange=0
-				shift
-				;;
-			--)
-				shift
-				break
-				;;
-			-*)
-				echo "$FUNCNAME: invalid option -- ${1:-}" >&2
-				return 1
-				;;
-			*)
-				break
-				;;
+			-z | --zero) yrange=0 && shift ;;
+			--) shift && break ;;
+			-*) echo "$FUNCNAME: illegal option -- ${1:-}" >&2 && return 1 ;;
+			*) break ;;
 		esac
 	done
 
@@ -136,6 +218,77 @@ plot() {
 	gnuplot "${args[@]}" -e "plot '$file' using $range with $style"
 }
 
+# Repeat a string n times
+repeat() {
+	local n
+
+	if [ "$#" -lt 2 ]; then
+		echo "$FUNCNAME: invalid usage" >&2
+		return 1
+	fi
+
+	n=$1 && shift
+
+	for i in $(seq $n); do
+		printf "%s" "$@"
+	done
+
+	printf "\n"
+}
+
+# Search diverse type of files
+search() {
+	local cmd
+	local dir
+	local reverse
+
+	# Get parameters
+	if [ -z "${1:-}" ]; then
+		echo "$FUNCNAME: missing command" >&2
+		return 1
+	fi
+
+	cmd=${1:-} && shift
+	dir=${1:-.} && shift
+
+	case "$cmd" in
+		crlf)
+			# Files without NL at end of file
+			find "$dir" "$@" -type f -exec grep -q "$(printf \\r)" {} \; -print
+			;;
+		empty)
+			# Empty files or directories
+			find "$dir" "$@" -empty
+			;;
+		largest | smallest)
+			# Largest or smallest files
+			[ "$cmd" = "largest" ] && reverse=true
+			find "$dir" "$@" -printf "%s\t%p\n" | sort -n ${reverse:+-r}
+			;;
+		latest | oldest)
+			# Most or less recently modified files
+			[ "$cmd" = "latest" ] && reverse=true
+			find "$dir" "$@" -printf "%TFT%.8TT %p\n" | sort ${reverse:+-r}
+			;;
+		nonl)
+			# Files without NL at end of file
+			find "$dir" "$@" -type f -not -exec sh -c '[ -z "$(tail -c1 "$0")" ]' {} \; -print
+			;;
+		today)
+			# Files modified today
+			find "$dir" "$@" -daystart -mtime -1
+			;;
+		ws)
+			# Files with trailing whitespaces
+			find "$dir" "$@" -type f -exec grep -q "[[:space:]]$" {} \; -print
+			;;
+		*)
+			echo "$FUNCNAME: illegal command -- $cmd" >&2
+			return 1
+			;;
+	esac
+}
+
 # Hash a file line by line
 shaline() {
 	local file=${1:-}
@@ -151,29 +304,54 @@ shaline() {
 	done
 }
 
-# Swap (or select) fields
-swap() {
-	local left=${1:--1}
-	local right=${2:-0}
-
-	# Normalize if negative value
-	[ "$left" -lt 0 ] && left="NF + $left"
-	[ "$right" -lt 0 ] && right="NF + $right"
-
-	# 0-based index
-	awk "{ print \$($left + 1),\$($right + 1) }"
+# Join strings with delimiter
+strjoin() {
+	local IFS="$1"
+	shift
+	echo "$*"
 }
 
-# Display today's date in sort of ISO 8601 format
-today() {
-	local format="%F"
+# Transpose lines into columns
+transpose() {
+	local OPTIND
+	local OPTARG
+	local opt
+	local d
+	local -a args=()
 
-	case "${1:-}" in
-		-f | --full) format="%FT%T" && shift ;;
-		-i | --iso) format="%FT%T%:z" && shift ;;
-	esac
+	# Parse options
+	while getopts ":d:" opt; do
+		case "$opt" in
+			d) d=$OPTARG ;;
+			:) echo "$FUNCNAME: option requires an argument -- $OPTARG" && return 1 ;;
+			?) echo "$FUNCNAME: illegal option -- $OPTARG" && return 1 ;;
+		esac
+	done
 
-	date ${1:+--date "$1"} +"$format"
+	shift $((OPTIND - 1))
+
+	# Set input and output field separators
+	args+=(${d:+-v FS="'$d'"} ${d:+-v OFS="'$d'"})
+
+	# Transpose lines to columns
+	awk "${args[@]}" '
+		{
+			if (NF > cols) {
+				cols = NF
+			}
+
+			for (i = 1; i <= NF; ++i) {
+				data[i, NR] = $i
+			}
+		}
+		END {
+			for (i = 1; i <= cols; ++i) {
+				for (j = 1; j <= NR; ++j) {
+					printf "%s%s", data[i, j], (j == NR ? ORS : OFS)
+				}
+			}
+		}
+	' "$@"
 }
 
 # Download a X.509 certificate from an URL
@@ -185,29 +363,18 @@ x509_fetch() {
 	# Parse options
 	while [ "$#" -gt 0 ]; do
 		case "$1" in
-			-a | --all)
-				showcerts=all
-				shift
-				;;
+			-a | --all) showcerts=all && shift ;;
 			-s | --servername)
 				if [ -z "${2:-}" ]; then
-					echo "$FUNCNAME: option $1 requires an argument" >&2
+					echo "$FUNCNAME: option requires an argument -- ${1:-}" >&2
 					return 1
 				fi
 				servername=$2
 				shift 2
 				;;
-			--)
-				shift
-				break
-				;;
-			-*)
-				echo "$FUNCNAME: invalid option -- ${1:-}" >&2
-				return 1
-				;;
-			*)
-				break
-				;;
+			--) shift && break ;;
+			-*) echo "$FUNCNAME: illegal option -- ${1:-}" >&2 && return 1 ;;
+			*) break ;;
 		esac
 	done
 
@@ -216,8 +383,77 @@ x509_fetch() {
 	# Add default port to 443 if not specified
 	! echo "$host" | grep -qE ":[0-9]+$" && host+=":443"
 
+	# Get certificates
 	openssl s_client -connect "$host" \
 		${showcerts:+-showcerts} ${servername:+-servername "$servername"} \
 		< /dev/null \
 		| sed -n "/-----BEGIN CERTIFICATE-----/,/-----END CERTIFICATE-----/p"
+}
+
+# Select fields in lines
+xcut() {
+	local OPTIND
+	local OPTARG
+	local opt
+	local d
+	local f
+	local -a args=()
+
+	# Parse options
+	while getopts ":d:f:" opt; do
+		case "$opt" in
+			d) d=$OPTARG ;;
+			f) f=$OPTARG ;;
+			:) echo "$FUNCNAME: option requires an argument -- $OPTARG" && return 1 ;;
+			?) echo "$FUNCNAME: illegal option -- $OPTARG" && return 1 ;;
+		esac
+	done
+
+	shift $((OPTIND - 1))
+
+	# Set input and output field separators
+	args+=(-v FUNCNAME="$FUNCNAME")
+	args+=(-v f="${f:--1,1}")
+	args+=(${d:+-v FS="$d"} ${d:+-v OFS="$d"})
+
+	awk "${args[@]}" '
+		BEGIN {
+			len = 0
+			split(f, fields, ",")
+
+			for (i = 1; i <= length(fields); ++i) {
+				# index 0 is not valid
+				if (fields[i] == 0) {
+					printf "%s: fields are numbered from 1\n", FUNCNAME > "/dev/stderr"
+					exit 1
+				}
+
+				# Convert range into actual indices
+				if (fields[i] ~ /^[0-9]+-[0-9]$/) {
+					split(fields[i], range, "-")
+
+					if (range[1] > range[2]) {
+						printf "%s: invalid range -- %s\n", FUNCNAME, fields[i] > "/dev/stderr"
+						exit 1
+					}
+
+					for (j = range[1]; j <= range[2]; ++j) {
+						indices[++len] = j
+					}
+
+					continue
+				}
+
+				# Normal case
+				indices[++len] = fields[i]
+			}
+		}
+		{
+			for (i = 1; i <= length(indices); ++i) {
+				idx = (indices[i] >= 0) ? indices[i] : NF + indices[i] + 1
+				sep = (i == length(indices)) ? ORS : OFS
+				printf "%s%s", $(idx), sep
+			}
+		}
+	' "$@"
 }
